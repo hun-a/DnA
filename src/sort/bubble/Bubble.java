db25aa502551dcbddbbab1ec19bfb3bf2cd0711e
seunghun <seunghun_kim@cubrid.com>
2017-07-21 17:36:13 +0900

    implement bubble sort

--- src/sort/bubble/Bubble.java
+++ src/sort/bubble/Bubble.java
@@ -8,17 +8,33 @@ import static util.AlgorithmUtil.*;
 
 public class Bubble implements Algorithms {
   List<Integer> list;
+  Integer[] arr;
   public static void main(String ... args) {
-    
+    Bubble bubble = new Bubble();
+    measure(bubble);
   }
 
   @Override
   public void init() {
-    list = generateInitialList(() -> (int)((Math.random() * 100) + 1), 10);
+    int limit = 10_000;
+    list = generateInitialList(() -> (int)((Math.random() * limit * 100) + 1), limit);
+    arr = new Integer[list.size()];
+    list.toArray(arr);
   }
 
   @Override
   public void implement() {
+    int listLength = arr.length;
+    int boundary = listLength - 1;
 
+    for (int i = 0; i < listLength; i++) {
+      for (int j = 0; j < boundary - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          Integer temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
   }
 }
