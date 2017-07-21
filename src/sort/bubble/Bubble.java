45b4ff1fdc3d51282c12ea1ff5ba9be4a68d2e2c
seunghun <seunghun_kim@cubrid.com>
2017-07-21 18:33:20 +0900

    implement sorting direction

--- src/sort/bubble/Bubble.java
+++ src/sort/bubble/Bubble.java
@@ -3,6 +3,7 @@ package sort.bubble;
 import template.Algorithms;
 
 import java.util.List;
+import java.util.function.BiPredicate;
 
 import static util.AlgorithmUtil.*;
 
@@ -16,7 +17,7 @@ public class Bubble implements Algorithms {
 
   @Override
   public void init() {
-    int limit = 10_000;
+    int limit = 1_000;
     list = generateInitialList(() -> (int)((Math.random() * limit * 100) + 1), limit);
     arr = new Integer[list.size()];
     list.toArray(arr);
@@ -24,12 +25,16 @@ public class Bubble implements Algorithms {
 
   @Override
   public void implement() {
+    bubbleSort((i, j) -> i > j);
+  }
+
+  private void bubbleSort(BiPredicate<Integer, Integer> test) {
     int listLength = arr.length;
     int boundary = listLength - 1;
 
     for (int i = 0; i < listLength; i++) {
       for (int j = 0; j < boundary - i; j++) {
-        if (arr[j] > arr[j + 1]) {
+        if (test.test(arr[j], arr[j + 1])) {
           Integer temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
