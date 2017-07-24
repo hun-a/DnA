df5c7c7f90c0d5d2c5f760a5bc695cc9289a9d22
seunghun <seunghun_kim@cubrid.com>
2017-07-24 11:27:16 +0900

    changed interface apply to Bubble.java

--- src/sort/bubble/Bubble.java
+++ src/sort/bubble/Bubble.java
@@ -2,44 +2,33 @@ package sort.bubble;
 
 import template.Algorithms;
 
-import java.util.List;
 import java.util.function.BiPredicate;
 
-import static util.AlgorithmUtil.*;
-
 public class Bubble implements Algorithms {
-  List<Integer> list;
-  Integer[] arr;
-  public static void main(String ... args) {
-    Bubble bubble = new Bubble();
-    measure(bubble);
+
+  @Override
+  public <T> void implement(T[] arr, BiPredicate<T, T> predicate) {
+    bubbleSort(arr, predicate);
   }
 
   @Override
-  public void init() {
-    int limit = 1_000;
-    list = generateInitialList(() -> (int)((Math.random() * limit * 100) + 1), limit);
-    arr = new Integer[list.size()];
-    list.toArray(arr);
+  public <T> T[] init() {
+    return null;
   }
 
-  @Override
-  public void implement() {
-    bubbleSort((i, j) -> i > j);
-  }
-
-  private void bubbleSort(BiPredicate<Integer, Integer> test) {
+  private <T> T[] bubbleSort(T[] arr, BiPredicate<T, T> test) {
     int listLength = arr.length;
     int boundary = listLength - 1;
 
     for (int i = 0; i < listLength; i++) {
       for (int j = 0; j < boundary - i; j++) {
         if (test.test(arr[j], arr[j + 1])) {
-          Integer temp = arr[j];
+          T temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
         }
       }
     }
+    return arr;
   }
 }
