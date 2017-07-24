329c9b7f77774694c40741aab4cf8f155d36872c
seunghun <seunghun_kim@cubrid.com>
2017-07-24 17:11:04 +0900

    add test for sorting by desc

--- src/test/sort/SortTest.java
+++ src/test/sort/SortTest.java
@@ -14,18 +14,28 @@ import static org.hamcrest.CoreMatchers.*;
  * Created by hun on 24/07/2017.
  */
 public class SortTest {
+  final Integer[] arr = {92, 57, 1, 78, 5, 9, 48, 22};
 
   @Test
-  public void bubbleTest() {
-    final Integer[] arr = {92, 57, 1, 78, 5, 9, 48, 22};
-    final Integer[] resultArr;
+  public void bubbleAscTest() {
 
     Bubble<Integer> bubble = new Bubble<>();
-    bubble.implement(arr, bubble::asc);
-    resultArr = Stream.of(arr).sorted().toArray(Integer[]::new);
+    Integer[] result = Stream.of(arr).sorted().toArray(Integer[]::new);
     bubble.implement(arr, bubble::asc);
 
     IntStream.rangeClosed(0, arr.length - 1)
-      .forEach((i) -> assertThat(arr[i], is(resultArr[i])));
+      .forEach((i) -> assertThat(arr[i], is(result[i])));
+  }
+
+  @Test
+  public void bubbleDescTest() {
+    Bubble<Integer> bubble = new Bubble<>();
+    Integer[] result = Stream.of(arr)
+      .sorted((i, j) -> j.intValue() - i.intValue())
+      .toArray(Integer[]::new);
+    bubble.implement(arr, bubble::desc);
+
+    IntStream.rangeClosed(0, arr.length - 1)
+      .forEach((i) -> assertThat(arr[i], is(result[i])));
   }
 }
