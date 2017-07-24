e831894fd6e0d618229cd73a00ff68ea90c0f5a7
seunghun <seunghun_kim@cubrid.com>
2017-07-24 13:40:05 +0900

    add junit test for bubble sort

---
+++ src/test/sort/SortTest.java
@@ -0,0 +1,30 @@
+package test.sort;
+
+import org.junit.*;
+import sort.bubble.Bubble;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * Created by hun on 24/07/2017.
+ */
+public class SortTest {
+
+  @Test
+  public void bubbleTest() {
+    final Integer[] arr = {92, 57, 1, 78, 5, 9, 48, 22};
+    final Integer[] resultArr;
+
+    Bubble<Integer> bubble = new Bubble<>();
+    bubble.implement(arr, bubble::asc);
+    resultArr = Stream.of(arr).sorted().toArray(Integer[]::new);
+    bubble.implement(arr, bubble::asc);
+
+    IntStream.rangeClosed(0, arr.length - 1)
+      .forEach((i) -> assertThat(arr[i], is(resultArr[i])));
+  }
+}
