196cfc1369b58ecb9edc6134c0afa5d93183483d
seunghun <seunghun_kim@cubrid.com>
2017-07-24 11:21:50 +0900

    change util by changing interface

--- src/util/AlgorithmUtil.java
+++ src/util/AlgorithmUtil.java
@@ -2,22 +2,13 @@ package util;
 
 import template.Algorithms;
 
-import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.*;
 
 /**
 * Created by hun on 21/07/2017.
 */
 public class AlgorithmUtil {
-  public static <T> List<T> generateInitialList(Supplier<T> random, int limit) {
-    return Stream.generate(random)
-        .distinct()
-        .limit(limit)
-        .collect(toList());
-  }
 
   public static <T> void print(T t, String comment) {
     System.out.print(comment + ": ");
@@ -27,11 +18,10 @@ public class AlgorithmUtil {
 
   public static <T> void measure(Algorithms algorithm) {
     long fastest = Long.MAX_VALUE;
-    algorithm.init();
-
+    T[] arr = algorithm.init();
     for (int i = 0; i < 10; i++) {
       long start = System.nanoTime();
-      algorithm.run();
+      algorithm.implement(arr, algorithm::asc);
       long duration = (System.nanoTime() - start) / 1_000_000;
       if (duration < fastest) {
         fastest = duration;
