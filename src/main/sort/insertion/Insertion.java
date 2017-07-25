437c20ec5adb6b3f6d3b00d1d9a4558912354e38
seunghun <seunghun_kim@cubrid.com>
2017-07-25 17:30:13 +0900

    implement Insertion sort

--- src/main/sort/insertion/Insertion.java
+++ src/main/sort/insertion/Insertion.java
@@ -8,13 +8,23 @@ import java.util.function.BiPredicate;
  * Created by hun on 25/07/2017.
  */
 public class Insertion<T> implements Algorithms<T> {
-
   @Override
   public <T> void implement(T[] arr, BiPredicate<T, T> predicate) {
     insertionSort(arr, predicate);
   }
 
   private <T> void insertionSort(T[] arr, BiPredicate<T, T> predicate) {
-
+    int length = arr.length;
+    for (int i = 1; i < length; i++) {
+      for (int j = i, compareIndex = (i - 1); j > 0; j--, compareIndex--) {
+        if (!predicate.test(arr[compareIndex], arr[j])) {
+          break;
+        } else {
+          T temp = arr[compareIndex];
+          arr[compareIndex] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
   }
 }
