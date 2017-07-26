d16de72e6204bbf7d067d6b1c2c05d2306f90e48
seunghun <seunghun_kim@cubrid.com>
2017-07-26 18:18:01 +0900

    implement shell sort

--- src/main/sort/shell/Shell.java
+++ src/main/sort/shell/Shell.java
@@ -14,6 +14,22 @@ public class Shell<T> implements Algorithms<T> {
   }
 
   private <T> void shellSort(T[] arr, BiPredicate<T, T> predicate) {
-
+    int gap = arr.length;
+    do {
+      gap = gap / 3 + 1;
+      for (int i = 0; i < gap; i++) {
+        for (int separatedArrayIndex = i + gap; separatedArrayIndex < arr.length; separatedArrayIndex += gap) {
+          for (int insertionIndex = separatedArrayIndex; insertionIndex - gap >= 0; insertionIndex--) {
+            if (!predicate.test(arr[insertionIndex - gap], arr[insertionIndex])) {
+              break;
+            } else {
+              T temp = arr[insertionIndex - gap];
+              arr[insertionIndex - gap] = arr[insertionIndex];
+              arr[insertionIndex] = temp;
+            }
+          }
+        }
+      }
+    } while (gap != 1);
   }
 }
