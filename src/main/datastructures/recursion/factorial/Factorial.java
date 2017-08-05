c80f16be37d5df2d06a7f378a1450fdfedf5bb1f
seunghun <seunghun.chan@gmail.com>
2017-08-06 02:43:17 +0900

    implement factorial and unit test

---
+++ src/main/datastructures/recursion/factorial/Factorial.java
@@ -0,0 +1,15 @@
+package main.datastructures.recursion.factorial;
+
+/**
+ * Created by hun on 06/08/17.
+ */
+public class Factorial {
+
+    public int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
---
+++ src/test/datastructures/recursion/RecursionTest.java
@@ -0,0 +1,24 @@
+package test.datastructures.recursion;
+
+import main.datastructures.recursion.factorial.Factorial;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * Created by hun on 06/08/17.
+ */
+public class RecursionTest {
+
+    @Test
+    public void factorialTest() {
+        Factorial factorial = new Factorial();
+
+        int n = 10;
+        int expect = 3628800;
+        int result = factorial.factorial(n);
+
+        assertThat(result, is(expect));
+    }
+}
