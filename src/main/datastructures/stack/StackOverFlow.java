7bb138a555037fc1e4762ba01198c3ca30ab2d88
seunghun <seunghun_kim@cubrid.com>
2017-08-09 17:51:05 +0900

    add exception class for stack

---
+++ src/main/datastructures/stack/StackOverFlow.java
@@ -0,0 +1,10 @@
+package main.datastructures.stack;
+
+/**
+ * Created by hun on 09/08/2017.
+ */
+public class StackOverFlow extends Exception {
+  public StackOverFlow() {
+    super("Stack Over Flow!");
+  }
+}
---
+++ src/main/datastructures/stack/StackUnderFlow.java
@@ -0,0 +1,10 @@
+package main.datastructures.stack;
+
+/**
+ * Created by hun on 09/08/2017.
+ */
+public class StackUnderFlow extends Exception {
+  public StackUnderFlow() {
+    super("Stack Under Flow!");
+  }
+}
